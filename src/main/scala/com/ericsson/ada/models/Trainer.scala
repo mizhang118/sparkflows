c9980a653d13a3510a911aa1704484a2ce6769a4
mizhang118 <mizhang118@gmail.com>
2017-07-19 16:02:16 -0700

    make a workable branch

---
+++ src/main/scala/com/ericsson/ada/models/Trainer.scala
@@ -0,0 +1,15 @@
+package com.ericsson.ada.models
+
+//import org.apache.spark.SparkContext
+//import org.apache.spark.h2o.H2OContext
+//import org.apache.spark.sql.SQLContext
+
+class Trainer {
+  
+}
+
+object ModelNode {
+//  var sparkContext: SparkContext = null
+//  var sqlContext: SQLContext = null
+//  var h2oContext: H2OContext = null
+}
